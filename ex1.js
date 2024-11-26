9abf2c78e58240e82bc6fe75cb8e3b69da59699d
shrutinimai <nimaishruti@gmail.com>
2024-11-26 14:11:48 +0530

    Add files via upload
    practising

---
+++ ex1.js
@@ -0,0 +1,11 @@
+//create a variable named price and set it to a specific value
+//create a variable named product and set it to a specific value
+//create a variable named tax and set it to a specific value
+//print the name of the product
+//calculate total price amt including tax,(total=price+tax)and then print it.
+let price=30
+let product="soap"
+let tax=10
+console.log(product)
+let total=price+tax
+console.log(total)
